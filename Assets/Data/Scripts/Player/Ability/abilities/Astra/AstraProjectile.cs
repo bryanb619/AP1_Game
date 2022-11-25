89e113b1201b9f3970c2c5697f5c59e299c66d1b
Steven Hall <stevenbryanb.work@gmail.com>
2022-11-25 17:04:10 +0000

    AI Update

--- Assets/Data/Scripts/Player/Ability/abilities/Astra/AstraProjectile.cs
+++ Assets/Data/Scripts/Player/Ability/abilities/Astra/AstraProjectile.cs
@@ -2,30 +2,37 @@ using UnityEngine;
 
 public class AstraProjectile : MonoBehaviour
 {
-    private float Bspeed = 20f;
-    //private int Bdamage = 20;
+    private float _speed = 20f;
+
+    private int EnemyDamage = 20;
+    public int enemyDamage => EnemyDamage;
+
+    private int EnemyChaseDamage = 80;
+    public int enemyChaseDamage => EnemyChaseDamage;
+
     [SerializeField] private Rigidbody rb;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb.velocity = transform.forward * Bspeed;
+        rb.velocity = transform.forward * _speed;
     }
 
     public void OnTriggerEnter(Collider hitInfo)
     {
         EnemyBehaviour enemy = hitInfo.GetComponent<EnemyBehaviour>();
+        EnemyChaseBehaviour ChaseEnemy = hitInfo.GetComponent<EnemyChaseBehaviour>();
+
         if (enemy != null)
         {
-            enemy.TakeDamage(20);
+            enemy.TakeDamage(enemyDamage);
             DestroyBullet();
-            Debug.Log("HIT");
+            //Debug.Log("HIT");
 
         }
-        EnemyChaseBehaviour Chaseenemy = hitInfo.GetComponent<EnemyChaseBehaviour>();
-        if (enemy != null)
+        else if (ChaseEnemy != null)
         {
-            enemy.TakeDamage(20);
+            ChaseEnemy.TakeDamage(enemyChaseDamage);
             DestroyBullet();
             Debug.Log("HIT");
 
@@ -33,6 +40,7 @@ public class AstraProjectile : MonoBehaviour
 
         //Instantiate(impactEffect, transform.position, transform.rotation);
     }
+
 
     private void DestroyBullet()
     {
