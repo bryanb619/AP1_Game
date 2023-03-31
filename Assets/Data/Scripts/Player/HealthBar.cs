286a0f6b27410d8b2388897d77d9d00b3e7d6199
DiogoF22104684 <a22104684@alunos.ulht.pt>
2023-03-31 13:50:54 +0100

    UI changes

--- Assets/Data/Scripts/Player/HealthBar.cs
+++ Assets/Data/Scripts/Player/HealthBar.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider _slider;
+    public Slider _slider, criticalSlider;
 
+    [SerializeField]
+    private float criticalSliderVelocity;
+    private float time;
+        
     //public Gradient _gradient;
 
     public Image _fill;
+
+    private void Update()
+    {
+        SetCriticalHealth();
+    }
 
     public void SetMaxHealth(int health)
     {
@@ -21,8 +30,15 @@ public class HealthBar : MonoBehaviour
 
     public void SetHealth(int health)
     {
+        time = 0f;
         _slider.value = health;
-
         //_fill.color = _gradient.Evaluate(_slider.normalizedValue);
+    }
+
+    private void  SetCriticalHealth()
+    {
+        time += Time.deltaTime * criticalSliderVelocity;
+
+        criticalSlider.value = Mathf.Lerp(criticalSlider.value, _slider.value, time);
     }
 }
