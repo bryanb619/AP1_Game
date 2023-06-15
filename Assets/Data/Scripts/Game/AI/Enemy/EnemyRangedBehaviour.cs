53ae1ee550306c82df225dba8184e10bb3328ebd
Steven Hall <stevenbryanb.work@gmail.com>
2023-06-15 15:48:31 +0100

    Ranged fixes

--- Assets/Data/Scripts/Game/AI/Enemy/EnemyRangedBehaviour.cs
+++ Assets/Data/Scripts/Game/AI/Enemy/EnemyRangedBehaviour.cs
@@ -92,7 +92,7 @@ public class EnemyRangedBehaviour : MonoBehaviour
            
     // Health/Death --------------------------------------------------------------------------------------------------->
     
-                    private AiHealth                            _aiHealth;    
+                    private AiHealth                            _healthBar;    
                     // HEALTH
                     public float                               health;
 
@@ -224,7 +224,7 @@ private float _stunnedTime;
         agent                       = GetComponent<NavMeshAgent>(); 
         _aiShoot                    = GetComponentInChildren<AiShoot>();
         _aiHandler                  = GetComponent<AIHandler>();
-        _aiHealth                   = GetComponentInChildren<AiHealth>();
+        _healthBar                  = GetComponentInChildren<AiHealth>();
         
         // mesh 
         _mesh                       = GetComponentInChildren<SkinnedMeshRenderer>();
@@ -304,11 +304,7 @@ private float _stunnedTime;
         
     }
     // Start is called before the first frame update
-    private void Start()
-    {
-       
-    }
-
+    
     private void StateSet()
     {
         // Create the states
@@ -329,6 +325,11 @@ private float _stunnedTime;
         
         // Create the state machine
         _fsm = new StateMachine(chaseState);
+    }
+    
+    private void Start()
+    {
+        _healthBar.HealthValueSet(_health);
     }
 
     // Update is called once per frame
@@ -412,41 +413,17 @@ private float _stunnedTime;
 
     private void Engage()
     {
-        switch (_enemyType)
-        {
-            case EnemyType.Normal:
-            {
-                if (_canAttack)
-                {
-                    Attack();
-                    UpdatePath();
-                }     
-                
-                
-                UpdateRotation(); 
-                break;
-            }
-            case EnemyType.Boss:
-            {
-                if (_canAttack)
-                {
-                    BossAttack();
-                    
-                    UpdateRotation(); 
-                }            
-                break;
-            }
-
-            default:{throw new ArgumentOutOfRangeException();}
-                
-        }
-        
+        if (!_canAttack) return;
+        Attack();
+        UpdateRotation();
     }   
     
      private void Attack()
      {
          if ((_playerTarget.transform.position - transform.position).magnitude >= _attackRange)
          {
+             UpdatePath();
+
              if (Time.time > _nextFire)
              {
                  //float randomFloat = UnityEngine.Random.value;
@@ -473,6 +450,8 @@ private float _stunnedTime;
             
          }
      }
+
+     #region Agent Pathfinding & Rotation
      
      private void UpdatePath()
      {
@@ -485,9 +464,6 @@ private float _stunnedTime;
              // set destination
              agent.SetDestination(_playerTarget.position);
          }
-         
-       
-
      }
      
      private void UpdateRotation()
@@ -501,7 +477,7 @@ private float _stunnedTime;
          transform.rotation = disaredRot;
      }
      
-    
+     #endregion
     
      private void NormalAttack()
     {
@@ -532,203 +508,24 @@ private float _stunnedTime;
                 Debug.Log(debugAttack + "Attack 2: " + closeAttack);
 #endif
      }
-
-
-     private void BossAttack()
-     { 
-         switch(_canSpecialAttack)
-        {
-            case true:
-                {
-                    
-                    if ((_playerTarget.transform.position - transform.position).magnitude >= _attackRange)  //
-                    {
-                        PauseAi();
-                        if (_canAttack) { SpecialAttack();}
-                    }
-                    
-                    /*
-                    else if ((_playerTarget.transform.position - transform.position).magnitude < 
-                             _attackRange && !_canSpecialAttack) //|| currentAbilityValue < ABILITY_MAX_VALUE)
-                    {
-                        if(_canAttack && _currentState == HandleState.None) 
-                        { 
-                            //ResumeAi();
-                            //GetDistance(9F);
-                  
-                        }
-                    }
-            */
-                    break;
-                }
-                
-            case false:
-                {
-                    
-                    if ((_playerTarget.transform.position 
-                         - transform.position).magnitude >= 15f)
-                    {
-                        if(_canAttack) 
-                        { 
-                            ResumeAi();
-                            UpdatePath();
-                        }
-                    }
-                    else if ((_playerTarget.transform.position 
-                              - transform.position).magnitude <= 9) //_attackRange && !_canSpecialAttack) 
-                    {
-                        GenerateRandomPos();
-                    }
-                    
-                    else if ((_playerTarget.transform.position - transform.position).magnitude >= 13) // valor mais alto 
-                    {
-                        //var position = _playerTarget.position;
-                        
-                        PauseAi();
-
-                        if(_canAttack && !_canSpecialAttack) { Attack(); }
-                    }
-                    
-                    CoolDoownPower();
-                    break; 
-                }
-        }
-     }
      
-
-    private void SpecialAttack()
-    {
-        PauseAi();   
-        
-        _aiShoot.Shoot(_playerTarget, _specialProjectile);
-        
-        _currentAbilityValue = 0;
-        //abilitySlider.value = _currentAbilityValue;
-        //_canSpecialAttack = false;
-        
-        _animator.SetBool("isAttacking", false);
-       // StartCoroutine(SpecialAttackTimer());
-        
-#if UNITY_EDITOR                                                                              
-                                                                                              
-        const string debugAttack = "<size=12><color=purple>";                                 
-        const string closeAttack = "</color></size>";                                         
-        Debug.Log(debugAttack + "Special Attack: " + closeAttack);                            
-                                                                                              
-#endif                                                                                        
-    }
-
-
-    private void CoolDoownPower()
-    {
-
-        if (_currentAbilityValue >= AbilityMaxValue)
-        {
-            _canSpecialAttack = true;
-        }
-        else
-        {
-            _currentAbilityValue = Mathf.Clamp(_currentAbilityValue + (_abilityIncreasePerFrame * Time.deltaTime), 0.0f, AbilityMaxValue);   
-        }
-
-        //abilitySlider.value = _currentAbilityValue;
-        //print(currentAbilityValue);
-    }
-/*
-    private IEnumerator SpecialAttackTimer()
-    {
-        bool isruning = false; 
-
-        if(!isruning) 
-        {
-            isruning = true;
-            _canPeformAttack = false;
-            _canAttack = false;
-            _activeDizzy = true; 
-            yield return new WaitForSeconds(4f);
-            _activeDizzy = false;
-            _canAttack = true;
-            _canPeformAttack = true;
-            ResumeAgent();  
-            isruning = false;
-        }
-        
-    }
-    */
-
-/*
-
-    private IEnumerator AttackTimer()
-    {
-        bool isruning = false;
-
-        if (!isruning)
-        {
-            isruning = true;
-            _canPeformAttack = false;
-            yield return new WaitForSeconds(1f);
-            _canPeformAttack = true;
-            isruning = false;
-        }
-
-
-    }
-    */
-
-    private void GenerateRandomPos()
-    {
-        if(Time.time > _nextTeleport)
-        {
-            TeleportProcess(); 
-            _nextFire = Time.time + teleportRate;
-        }
-        
-    }
-
-    private void TeleportProcess()
-    {
-
-#if UNITY_EDITOR
-        
-        print("running");
-#endif
-        
-        
-        Vector3 randomDirection = Random.insideUnitSphere * Random.Range(_minRange, _maxRange);
-
-        randomDirection += _playerTarget.position;
-
-        NavMeshHit hit;
-
-        NavMesh.SamplePosition(randomDirection, out hit, _maxRange, 1);
-
-        Vector3 teleportPos = hit.position;
-
-        StartCoroutine(Teleport(teleportPos));
-    }
-
-    private IEnumerator Teleport(Vector3 teleportPos)
-    {
-        agent.enabled = false;
-        _canAttack = false;
-        
-        Instantiate(_teleportEffect, teleportPos, Quaternion.identity);
-        
-        yield return new WaitForSeconds(1f);
-        transform.position = teleportPos;
-        
-        yield return new WaitForSeconds(0.5f);
-        agent.enabled = true;
-        _canAttack = true;
-    }
-    
-    
-
+     
     #endregion
     
     #region Health
     public void TakeDamage(int damage, WeaponType type, int damageBoost)
     {
+<<<<<<< HEAD
+         if (_health > 0)
+         {
+             switch (type)
+             {
+                 case WeaponType.Normal:
+                 {
+                     _health -= damage + damageBoost;
+
+                     //StartCoroutine(HitFlash());
+=======
         if (health <= 0)
         {
             Die();
@@ -741,9 +538,27 @@ private float _stunnedTime;
                 case WeaponType.Normal:
                     {
                         health -= damage + damageBoost;
+>>>>>>> origin/main
 
-                        //StartCoroutine(HitFlash());
+                     //damageEffectTime = 2f; 
+                     break;
+                 }
 
+<<<<<<< HEAD
+                 case WeaponType.Ice:
+                 {
+                     if (_shooterScript.WUpgraded == true)
+                     {
+                         //StartCoroutine(DamageOverTime(_damageOverTime, _durationOfDot));
+                         StartCoroutine(HitFlash());
+                     }
+                     else
+                     {
+                         _health -= damage + damageBoost;
+                         //Instantiate(targetEffect, transform.position, transform.rotation);
+                         StartCoroutine(HitFlash());
+                     }
+=======
                         //damageEffectTime = 2f; 
                         break;
                     }
@@ -761,58 +576,71 @@ private float _stunnedTime;
                             //Instantiate(targetEffect, transform.position, transform.rotation);
                             //StartCoroutine(HitFlash());
                         }
+>>>>>>> origin/main
                         
-                        break;
-                    }
+                     break;
+                 }
 
+<<<<<<< HEAD
+                 case WeaponType.Fire:
+                 {
+                     _health -= damage + damageBoost;
+=======
                 case WeaponType.Fire:
                     {
                         health -= damage + damageBoost;
+>>>>>>> origin/main
 
-                        //StartCoroutine(HitFlash());
+                     StartCoroutine(HitFlash());
 
-                        //damageEffectTime = 2.3f;
+                     //damageEffectTime = 2.3f;
 
-                        break;
-                    }
+                     break;
+                 }
 
+<<<<<<< HEAD
+                 case WeaponType.Thunder:
+                 {
+                     _health -= damage + damageBoost;
+=======
                 case WeaponType.Thunder:
                     {
                         health -= damage + damageBoost;
+>>>>>>> origin/main
 
-                        if (_shooterScript.RUpgraded == true)
-                            StartCoroutine(StopForSeconds(_stunnedTime));
-                        else
-                            StartCoroutine(HitFlash());
-                        break;
-                    }
+                     if (_shooterScript.RUpgraded == true)
+                         StartCoroutine(StopForSeconds(_stunnedTime));
+                     else
+                         StartCoroutine(HitFlash());
+                     break;
+                 }
 
+<<<<<<< HEAD
+                 case WeaponType.Dash:
+                 {
+                     _health -= damage + damageBoost;
+=======
                 case WeaponType.Dash:
                     {
                         health -= damage + damageBoost;
+>>>>>>> origin/main
 
-                        StartCoroutine(HitFlash());
-                        break;
-                    }
-            }
+                     StartCoroutine(HitFlash());
+                     break;
+                 }
+             }
 
-            if (_spawnHealth || _spawnMana)
-            {
-                DropSpawnCheck();
-            }
+             if (_spawnHealth || _spawnMana)
+             {
+                 DropSpawnCheck();
+             }
             
-            _damageText.text = damage.ToString();
-            StartCoroutine(DamageTextDisappear());
-
-            //healthSlider.value = _health;
-            //_healthBar.HandleBar(damage);
+             //  CALCULATE HEALTH 
+             // ADD DEATH
             
-            if (_canAttack)
-            {
-                //_warn.CanAlertAi = true;
-                //SetAttack();
-            }
-        }
+             _damageText.text = damage.ToString();
+             StartCoroutine(DamageTextDisappear());
+         }
     }
     
     
@@ -918,36 +746,35 @@ private float _stunnedTime;
 
     #region Death
 
-    
-
-   
-    private void Die()
+    public void Die()
     {
-        if(_spawnHealth)
+        if (_health<=0)
         {
-            for (var i = 0; i < _healthItems; i++)
+            if(_spawnHealth)
             {
-                SpawnDrop(_healthDrop);
-            }   
-        }
-
-        if(_spawnMana)
-        {
-            for (var i = 0; i < _manaItems; i++)
-            {
-                SpawnDrop(_manaDrop);
+                for (var i = 0; i < _healthItems; i++)
+                {
+                    SpawnDrop(_healthDrop);
+                }   
             }
+
+            if(_spawnMana)
+            {
+                for (var i = 0; i < _manaItems; i++)
+                {
+                    SpawnDrop(_manaDrop);
+                }
+            }
+            
+                    
+            Instantiate(_deathEffect, transform.position, Quaternion.identity);
+
+            //_valuesTexts.GetKill();
+
+            _objectiveUiScript.IncreaseEnemyDefeatedCount();
+            
+            Destroy(gameObject);
         }
-        
-        
-        Instantiate(_deathEffect, transform.position, Quaternion.identity);
-
-        //_valuesTexts.GetKill();
-
-        _objectiveUiScript.IncreaseEnemyDefeatedCount();
-        
-
-        Destroy(gameObject);
     }
     
     #endregion   
