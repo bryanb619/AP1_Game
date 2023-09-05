db4d0035c2632a2218931b39bea0db97ccc83a98
DiogoF22104684 <a22104684@alunos.ulht.pt>
2023-09-05 12:20:32 +0100

    Sword combo implementation

--- Assets/Data/Scripts/Game/Player/PlayerAnimationHandler.cs
+++ Assets/Data/Scripts/Game/Player/PlayerAnimationHandler.cs
@@ -237,6 +237,11 @@ public class PlayerAnimationHandler : MonoBehaviour
         shooter.RAbilityTelegraphActivation();
     }
 
+    public void CastBasicAttack()
+    {
+        _playerAnimator.SetBool("BasicAttack", true);
+    }
+
     public void ResetBasicAttackVariable()
     {
         _playerAnimator.SetBool("BasicAttack", false);
